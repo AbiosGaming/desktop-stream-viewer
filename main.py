50577bd49ef212146b6470047bee127507d62357
Sven <sven@androd.se>
2017-02-18 11:17:34 +0100

    Changing to a proper GTK application instead.

--- main.py
+++ main.py
@@ -1,82 +1,129 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Example program of using libVLC to play online livestreams with full access
-to video buffer using Streamlink.
+# Example application for the Abios Gaming - Desktop Stream Viewer.
+# Sven Anderzén - 2017
 
-For more information about the Streamlink API visit:
-    https://streamlink.github.io/api_guide.html
-For more information about the libVLC API visit:
-    https://www.videolan.org/developers/vlc/doc/doxygen/html/index.html
-"""
-
+# GTK imports.
 import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("GdkX11", "3.0")
-from gi.repository import Gtk
-from gi.repository import GdkX11
+from gi.repository import GObject, Gio, Gtk, GdkX11
 
-import ctypes
-import shutil
 import sys
+import ctypes
 
 import vlc
 import streamlink
-import gui
 import callbacks as cb
 
-def main(stream_one_info, stream_two_info):
-    """Creates the application containing 2 separate VLC media players."""
+class Application(Gtk.Application):
+    
+    def __init__(self, application_id, flags, stream_info):
+        Gtk.Application.__init__(self, application_id = application_id, flags = flags)
+        self.connect("activate", self.activate)
 
-    # Open the StreamLink streams.
-    streams = streamlink.streams(stream_one_info["url"])
-    stream_one = streams[stream_one_info["quality"]].open()
-    streams = streamlink.streams(stream_two_info["url"])
-    stream_two = streams[stream_two_info["quality"]].open()
+        # Kick up a VLC instance. 
+        self.vlc_instance = vlc.Instance("--no-xlib")
+        
+        # Streamlink streams.
+        #self.stream_one = self.get_stream(stream_info[0])
+        #self.stream_two = self.get_stream(stream_info[1])
+        streams = streamlink.streams(stream_info[0]["url"])
+        self.stream_one = streams[stream_info[0]["quality"]].open()
+        streams = streamlink.streams(stream_info[1]["url"])
+        self.stream_two = streams[stream_info[1]["quality"]].open()
 
-    # Create our VLC media for the two streams.
-    instance = vlc.Instance("--no-xlib")
-    opaque_one = ctypes.cast(ctypes.pointer(ctypes.py_object(stream_one)), ctypes.c_void_p)
-    opaque_two = ctypes.cast(ctypes.pointer(ctypes.py_object(stream_two)), ctypes.c_void_p)
-    media_one = instance.media_new_callbacks(
-        cb.callbacks["read"],
-        cb.callbacks["open"],
-        cb.callbacks["seek"],
-        cb.callbacks["close"],
-        opaque_one
-    )
-    media_two = instance.media_new_callbacks(
-        cb.callbacks["read"],
-        cb.callbacks["open"],
-        cb.callbacks["seek"],
-        cb.callbacks["close"],
-        opaque_two
-    )
+        # Create media callbacks.
+        #self.media_one = self.create_media_callbacks(self.stream_one)
+        #self.media_two = self.create_media_callbacks(self.stream_two)
+        opaque_one = ctypes.cast(ctypes.pointer(ctypes.py_object(self.stream_one)), ctypes.c_void_p) 
+        self.media_one = self.vlc_instance.media_new_callbacks(
+            cb.callbacks["read"],
+            cb.callbacks["open"],
+            cb.callbacks["seek"],
+            cb.callbacks["close"],
+            opaque_one
+        )
+        opaque_two = ctypes.cast(ctypes.pointer(ctypes.py_object(self.stream_one)), ctypes.c_void_p) 
+        self.media_two = self.vlc_instance.media_new_callbacks(
+            cb.callbacks["read"],
+            cb.callbacks["open"],
+            cb.callbacks["seek"],
+            cb.callbacks["close"],
+            opaque_two
+        )
 
-    # Create the GTK window.
-    window = gui.ApplicationWindow(instance, media_one, media_two)
-    window.show_all()
+    def activate(self, *args):
+        ApplicationWindow(self)
 
-    # GTK main event loop.
-    Gtk.main()
+    def get_stream(self, stream_info):
+        streams = streamlink.streams(stream_info["url"])
+        stream = streams[stream_info["quality"]].open()
+        
+        return stream
 
-    # Destory the players and relese the VLC instance.
-    # window.player_one.stop()
-    # window.player_two.stop()
-    # window.vlc_instance.release()
+    def create_media_callbacks(self, stream):
+        opaque = ctypes.cast(ctypes.pointer(ctypes.py_object(stream)), ctypes.c_void_p) 
+        media = self.vlc_instance.media_new_callbacks(
+            cb.callbacks["read"],
+            cb.callbacks["open"],
+            cb.callbacks["seek"],
+            cb.callbacks["close"],
+            opaque
+        )
 
-if __name__ == "__main__":
+        return media
 
-    # Ghetto-check that VLC is installed.
-    if not shutil.which("vlc"):
-        sys.exit("Could not find VLC executable.")
+class ApplicationWindow(object):
+    
+    def __init__(self, application):
+        self.application = application
+
+        # Load Glade UI file.
+        try:
+            builder = Gtk.Builder.new_from_file("gui.glade")
+            builder.connect_signals(self)
+        except GObject.GError:
+            print("Could not read gui.glade.")
+            raise
+
+        self.window = builder.get_object("window")
+        self.window.set_application(application)
+
+        self.window.set_size_request(1280, 720)
+        self.window.show()
+
+    def onDrawReadyOne(self, widget,  *args):
+        player = self.application.vlc_instance.media_player_new()
+        xid = widget.get_window().get_xid()
+        player.set_xwindow(xid)
+        player.set_media(self.application.media_one)
+        player.play()
+
+    # Temporary workaround since we read stream_info from CLI and not GUI atm.
+    def onDrawReadyTwo(self, widget,  *args):
+        player = self.application.vlc_instance.media_player_new()
+        xid = widget.get_window().get_xid()
+        player.set_xwindow(xid)
+        player.set_media(self.application.media_two)
+        player.play()
+
+    def onDelete(self, *args):
+        self.application.vlc_instance.release()
+        self.window.destroy()
+
+def main():
 
     try:
-        stream_one_info, stream_two_info = {}, {}
-        stream_one_info["url"], stream_one_info["quality"] = sys.argv[1], sys.argv[2]
-        stream_two_info["url"], stream_two_info["quality"] = sys.argv[3], sys.argv[4]
-    except IndexError:
-        sys.exit("Usage: {0} <url> <quality> <url> <quality> ".format(__file__))
+        stream_info = [{}, {}]
+        stream_info[0]["url"], stream_info[0]["quality"] = sys.argv[1], sys.argv[2]
+        stream_info[1]["url"], stream_info[1]["quality"] = sys.argv[3], sys.argv[4]
+    except Exception as e:
+        sys.exit("Usage: {} <url> <quality> <url> <quality>".format(__file__))
 
-    main(stream_one_info, stream_two_info)
+    app = Application("com.abiosgaming", Gio.ApplicationFlags.FLAGS_NONE, stream_info)
+    app.run()
+
+if __name__ == "__main__":
+    main()
